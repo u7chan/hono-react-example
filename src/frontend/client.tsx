b580e2f0384835875814e8b0411a4a7f43591f75
u7chan <34462401+u7chan@users.noreply.github.com>
2024-07-29 23:07:02 +0900

    feat: form

--- src/frontend/client.tsx
+++ src/frontend/client.tsx
@@ -1,9 +1,35 @@
+import type { FormEvent } from 'react'
 import { createRoot } from 'react-dom/client'
 
 const App = () => {
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    const formValues = Array.from(new FormData(e.currentTarget).entries()).reduce(
+      (pre, [k, v]) => {
+        pre[k] = v
+        return pre
+      },
+      {} as { [k: string]: FormDataEntryValue },
+    )
+    console.log('#', formValues)
+  }
   return (
     <>
       <h1>Hello Hono with React SPA</h1>
+      <hr />
+      <form style={{ display: 'inline-flex', flexDirection: 'column' }} onSubmit={handleSubmit}>
+        <label htmlFor='username'>
+          <span>Username</span>
+        </label>
+        <input type='text' name='username' required />
+        <label htmlFor='password'>
+          <span>Password</span>
+        </label>
+        <input type='password' name='password' required />
+        <button type='submit' style={{ marginTop: 12 }}>
+          Login
+        </button>
+      </form>
     </>
   )
 }
